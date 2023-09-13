254ab188f15e1639fafc7d57e5a7de396285fdaf
akandeBolaji <akandebolajidanielofficial@gmail.com>
2023-09-13 02:31:19 +0100

    add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,31 +2,55 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\OrderStatus;
+use App\Models\Post;
+use App\Models\Product;
+use App\Models\Promotion;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Category::factory(10)->create();
-        \App\Models\Brand::factory(5)->create();
-        \App\Models\Product::factory(50)->create();
-        \App\Models\OrderStatus::factory(10)->create();
-        \App\Models\Order::factory(5)->create();
-        \App\Models\Payment::factory(5)->create();
-        \App\Models\JwtToken::factory(50)->create();
-        \App\Models\File::factory(10)->create();
-        \App\Models\Post::factory(5)->create();
-        \App\Models\Promotion::factory(5)->create();
+        //create the test admin
+        User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'Admin',
+            'email' => 'test_admin@example.com',
+            'is_admin' => true,
+            'password' => Hash::make('admin'), //admin
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //promotion seeders
+        Promotion::factory(20)->create();
+
+        //blog seeders
+        Post::factory(25)->create();
+
+        //category seeders
+        Category::factory(10)->create();
+
+        //brands seeders
+        Brand::factory(5)->create();
+
+        //product seeders
+        Product::factory(25)->create();
+
+        //order status seeders
+        $statuses = ['open', 'pending payment', 'paid', 'shipped', 'cancelled'];
+        foreach ($statuses as $status) {
+            OrderStatus::factory()->create(['title' => $status]);
+        }
+
+        User::factory()->count(10)->hasOrders(50)->create();
     }
 }
